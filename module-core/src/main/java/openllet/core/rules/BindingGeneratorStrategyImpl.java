d419bd0d9083c972618b3d106eff0d7a96091616
galigator <sejourne_kevin@yahoo.fr>
2017-06-04 12:40:38 +0200

    replication of bdionne changes

--- module-core/src/main/java/openllet/core/rules/BindingGeneratorStrategyImpl.java
+++ module-core/src/main/java/openllet/core/rules/BindingGeneratorStrategyImpl.java
@@ -124,7 +124,7 @@ public class BindingGeneratorStrategyImpl implements BindingGeneratorStrategy
 
 		helpers.addAll(new TrivialSatisfactionHelpers(_abox).getHelpers(rule));
 
-		if (!ensureOrdering(helpers, initialBinding))
+		if (!ensureOrdering(helpers))
 		{
 			BindingGenerator._logger.warning("IGNORING RULE " + rule + ": Could not generate safe ordering for body constraints.");
 			return new BindingGeneratorImpl();
@@ -137,7 +137,7 @@ public class BindingGeneratorStrategyImpl implements BindingGeneratorStrategy
 	/**
 	 * Reorder list so that each binding helper's prerequisites are satisfied by the helpers before it. If no such ordering exists, return false.
 	 */
-	private static boolean ensureOrdering(final List<BindingHelper> helpers, @SuppressWarnings("unused") final VariableBinding initialBinding)
+	private static boolean ensureOrdering(final List<BindingHelper> helpers)
 	{
 		final List<BindingHelper> unsatList = new ArrayList<>();
 		final Set<AtomVariable> bound = new HashSet<>();
