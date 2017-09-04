629e8fd2396d9cb7b199b9e3f55d1bd569f6ffc5
galigator <sejourne_kevin@yahoo.fr>
2017-09-04 22:53:25 +0200

    Rename/improve example with owl2 on jena.

--- examples/src/main/java/openllet/examples/JenaOwl2_Example.java
+++ examples/src/main/java/openllet/examples/JenaOwl2_Example.java
@@ -6,40 +6,26 @@ import openllet.jena.PelletReasonerFactory;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.ontology.OntProperty;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.util.iterator.ExtendedIterator;
 
-/**
- * TODO
- * 
- * @since
- */
-public class JenaTest
+public class JenaOwl2_Example
 {
 	public static void main(final String[] args)
 	{
 		final String ontostring = //
 				"@prefix : <http://purl.bdrc.io/ontology/> .\n" + //
-						"@prefix bdo: <http://purl.bdrc.io/ontology/> .\n" + //
+						"@prefix bdo: <http://bdo/> .\n" + //
 						"@prefix owl: <http://www.w3.org/2002/07/owl#> .\n" + //
 						"@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n" + //
-						"@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n" + //
-						"@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n" + //
 						"@base <http://purl.bdrc.io/ontology/> .\n" + //
-						"\n" + //
-						"<http://purl.bdrc.io/ontology/> rdf:type owl:Ontology ;\n" + //
-						"                                 rdfs:label \"BDRC Ontology\"^^xsd:string ;\n" + //
-						"                                 owl:versionInfo \"0.1\"^^xsd:string .\n" + //
-						"\n" + //
-						"bdo:personKinWho rdf:type owl:ObjectProperty ,\n" + //
-						"                          owl:FunctionalProperty ;\n" + //
-						"                 rdfs:domain bdo:Kin ;\n" + //
-						"                 rdfs:range bdo:Person .\n" + //
-						"\n" + //
-						"\n" + //
-						"bdo:Kin rdf:type owl:Class .\n" + //
-						"\n" + //
-						"bdo:Person rdf:type owl:Class .";
+						"<http://purl.bdrc.io/ontology/> rdf:type owl:Ontology .\n" + //
+						"bdo:x rdf:type owl:ObjectProperty , owl:FunctionalProperty .\n"//
+		;
+
 		final OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
 
 		final InputStream stream = new ByteArrayInputStream(ontostring.getBytes());
@@ -47,24 +33,37 @@ public class JenaTest
 		ExtendedIterator<OntProperty> ppi = m.listAllOntProperties();
 
 		System.out.println("iter or ppi");
-		int i = 0;
 		while (ppi.hasNext())
 		{
-			System.out.print(i++ + " ");
 			final OntProperty p = ppi.next();
 			System.out.println(p);
 		}
 		final OntModel ontoModelInferred = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC, m);
-		ontoModelInferred.setStrictMode(false);
+
+		// ------------------------------------------------------------------------------------------
+		ontoModelInferred.setStrictMode(false); // <--------------- HERE THE TRICK FOR OWL2. --------
+		// ------------------------------------------------------------------------------------------
+
 		ppi = ontoModelInferred.listAllOntProperties();
 
 		System.out.println("iter or ppi");
-		i = 0;
 		while (ppi.hasNext())
 		{
-			System.out.print(i++ + " ");
 			final OntProperty p = ppi.next();
 			System.out.println(p);
 		}
+
+		final Reasoner reasoner = PelletReasonerFactory.theInstance().create();
+		final Model emptyModel = ModelFactory.createDefaultModel();
+		final InfModel model = ModelFactory.createInfModel(reasoner, emptyModel);
+		model.read(stream, null, "TURTLE");
+
+		System.out.println("iter or ppi");
+		while (ppi.hasNext())
+		{
+			final OntProperty p = ppi.next();
+			System.out.println(p);
+		}
+
 	}
 }
